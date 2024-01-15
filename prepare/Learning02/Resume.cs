3456006f85aace8969e134a2f56caa6012654ae7
Langtanium <theminecraftsibs@gmail.com>
2024-01-15 15:44:41 -0800

    Finished Learning02 Program

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job _job in _jobs)
+        {
+            Console.WriteLine($"{_job._jobTitle} ({_job._company}) {_job._startYear}-{_job._endYear}");
+        }
+    }
+}
