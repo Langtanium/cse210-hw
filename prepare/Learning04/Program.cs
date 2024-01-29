4000b25df4ba3d1a3c87324d28c4a8e75629816d
Langtanium <theminecraftsibs@gmail.com>
2024-01-29 14:59:13 -0800

    Finished Week04 Learning Program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment mathHomework = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathHomework.GetSummary());
+        Console.WriteLine(mathHomework.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment englishHomework = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(englishHomework.GetSummary());
+        Console.WriteLine(englishHomework.GetWritingInformation());
     }
 }
