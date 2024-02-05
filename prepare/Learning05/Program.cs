bc00068fa8aa2addc70bfc4cf1f635ab861170fd
Langtanium <theminecraftsibs@gmail.com>
2024-02-05 14:10:06 -0800

    Finished Shape Program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>()
+        {
+            new Square("red", 2.5),
+            new Rectangle("green", 6.1, 3.4),
+            new Circle("blue", 7.8)
+        };
+
+        foreach (Shape shape in shapeList)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The shape is {color} and has an area of {area}");
+        }
     }
 }
